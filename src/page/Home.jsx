cbcccf37165dcf954b04391a665e052754a04f94
Kim-jaeyeon <jy020507@naver.com>
2024-08-01 02:44:08 +0900

    Feat[home제작, 미디어쿼리,useEffect]
    home페이지를 제작하였다. react-responsive 라이브러리를 활용하여 반응형을 제작하였고, useEffect를 통해 실시간 선택 배열 상태관리를 동작하도록 했다.  (페이지 전환은 아직 이루어지지 않은 상태)

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import {DotLottieReact} from "@lottiefiles/dotlottie-react";
+import React, { useState, useEffect } from 'react';
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import loading from '../assets/lz8ihbkj.lottie';
+import { useMediaQuery } from 'react-responsive';
 
 /**
  * Start 컴포넌트는 사용자에게 알파벳을 선택하고 분석할 수 있는 인터페이스를 제공합니다.
@@ -16,7 +17,14 @@ function Start() {
      * 선택된 알파벳을 추적하기 위한 상태
      * @type {Array}
      */
+    const isMobile = useMediaQuery({ query: '(max-width: 500px)' }); // 미디어 쿼리 라이브러리를 활용한 반응형 제작
     const [selectedParts, setSelectedParts] = useState([]);
+    const isAnySelected = selectedParts.length > 0;
+
+    useEffect(() => {
+        console.log(selectedParts);
+    
+    }, [selectedParts]); // 리액트는 비동기적으로 업데이트가 이루어지기에 이전 내용이 나올 수 있다. 따라서 배열이 업데이트 될 때마다 바로바로 렌더링 해주는 useEffect를 썼다
 
     /**
      * Whitebtn 컴포넌트의 클릭 이벤트 처리기
@@ -24,34 +32,8 @@ function Start() {
      */
     const handleWhiteBtnClick = (part) => {
         setSelectedParts((prev) =>
-            prev.includes(part) ? prev.filter(p => p !== part) : [...prev, part] //https://ko.react.dev/learn/updating-arrays-in-state
-            /*
-            ===================위 코드는 아래와 동일함===================
-            if (prev.includes(part)) {
-                return prev.filter(p => p !== part); // part가 포함되어 있으면 제거
-            } else {
-                return [...prev, part]; // part가 포함되어 있지 않으면 추가
-            }
-
-            ===================세부 설명===================
-
-            prev.filter(p => p !== part) : 새로운 요소를 제외한 새로운 배열을 반환
-
-            ...prev : 이전 상태의 배열 값들
-            part : 새로운 요소
-            [...prev, part] : 새로운 요소를 추가한 새로운 배열을 반환
-
-            ===================왜 prev.append(part)를 사용하지 않는가?===================
-
-            왜 prev.append(part)를 사용하지 않는가?
-            append()는 배열을 수정하는 메서드이므로, 원본 배열을 수정하게 된다.
-            하지만 React에서는 상태를 직접 수정하는 것을 허용하지 않는다.
-            따라서, append() 대신 새로운 배열을 반환하는 연산을 사용하는 것.
-            https://ko.react.dev/learn/updating-arrays-in-state 참고
-
-             */
+            prev.includes(part) ? prev.filter(p => p !== part) : [...prev, part]
         );
-        console.log(selectedParts);
     };
 
     /**
@@ -66,9 +48,11 @@ function Start() {
      * )
      */
     const Whitebtn = ({ text }) => {
+        const isSelected = selectedParts.includes(text);
+
         return (
             <button
-                className='hover:bg-blue-300 transition ease-in-out flex justify-center items-center w-40 h-14 m-4 rounded-xl border border-solid border-black bg-white text-black text-lg'
+                className={`hover:bg-blue-300 transition ease-in-out flex justify-center items-center w-48 h-14 m-4 rounded-full border border-solid border-black ${isSelected ? 'bg-[#007AFF] text-white' : isAnySelected ? 'bg-[#F5F5F5] text-black' : 'bg-white text-black'} text-lg`}
                 onClick={() => handleWhiteBtnClick(text)}
             >
                 {text}
@@ -87,10 +71,11 @@ function Start() {
      *   <Bluebtn text="입력하기" />
      * )
      */
-    const Bluebtn = ({ text }) => {
+    const Bluebtn = ({ hover, color, text }) => {
+        const isBlue=color=='bg-[#62ADFF]';
         return (
             <button
-                className='hover:bg-blue-300 transition ease-in-out flex justify-center items-center rounded-xl w-1/4 h-16 m-2 bg-blue-400 text-2xl'
+                className={`${hover} transition ease-in-out flex justify-center items-center rounded-full w-full h-16 m-2 ${isBlue? isAnySelected?'bg-[#007AFF]':color:color} ${isMobile ? 'text-xl' : 'text-2xl'}`}
             >
                 {text}
             </button>
@@ -101,43 +86,60 @@ function Start() {
      * 알파벳 배열
      * @type {Array<Array<string>>}
      */
-    const bodyPart = [['A-a', 'A-b', 'A-c'], ['B-a', 'B-b'], ['C-a', 'C-b', 'C-c'], ['D-a']];
+    const bodyPart = [['머리', '얼굴', '목'], ['가슴', '복부'], ['골반', '팔', '다리'], ['기타']];
 
     return (
-        <>
-            <div className="w-1/2 h-1/2">
-                <DotLottieReact
-                    src={loading}
-                    loop
-                    autoplay
-                    segments={[0, 12]}
-                    mode="reverse"
-                    useFrameInterpolation={false}
-                />
+        <div className='bg-[#F5F5F5]'>
+            {isMobile ? <div className='w-full h-[300px] flex justify-center'>
+                <div className=" w-[300px] h-full">
+                    <DotLottieReact
+                        src={loading}
+                        loop
+                        autoplay
+                        segments={[0, 12]}
+                        mode="reverse"
+                        useFrameInterpolation={false}
+                    />
+                </div>
             </div>
+                :
+                <div className='w-full h-[400px] flex justify-center'>
+                    <div className=" w-[400px] h-full">
+                        <DotLottieReact
+                            src={loading}
+                            loop
+                            autoplay
+                            segments={[0, 12]}
+                            mode="reverse"
+                            useFrameInterpolation={false}
+                        />
+                    </div>
+                </div>}
 
-            <section className="body-font text-white">
-                <div className='relative flex flex-col justify-center w-full h-screen bg-white'>
-                    <div className='absolute top-0 flex flex-col justify-center text-black items-center w-full h-40 mt-20'>
-                        <h1 className='ml-2 mt-2 text-4xl'>증상을 통해 <br/>내 몸의 문제를 분석해드려요</h1>
+
+
+            <section className="body-font text-white flex justify-center items-center h-full">
+                <div className='flex flex-col justify-between items-center w-11/12 h-full'>
+                    <div className='flex flex-col text-black items-center w-full h-40'>
+                        {isMobile ? <h1 className='ml-2 mt-2 text-3xl'>증상을 통해 <br />내 몸의 문제를 분석해드려요</h1> : <h1 className='ml-2 mt-2 text-4xl'>증상을 통해 <br />내 몸의 문제를 분석해드려요</h1>}
                         <p className='mt-5'>증상이 있는 부위를 선택해주세요.(중복가능)</p>
                     </div>
-                    <div className='w-full h-auto mt-10'>
+                    <div className='flex flex-col justify-center items-center w-full flex-grow'>
                         {bodyPart.map((part, index) => (
                             <div key={index} className='flex justify-center items-center w-full h-16 m-2'>
                                 {part.map((item, index) => (
-                                    <Whitebtn key={index} text={item}/>
+                                    <Whitebtn key={index} text={item} />
                                 ))}
                             </div>
                         ))}
                     </div>
-                    <div className='flex justify-center items-center flex-col w-full h-40 mt-10'>
-                        <Bluebtn text={"증상 입력하기"}/>
-                        <Bluebtn text={"분석 기록 / 예약 정보 확인하기"}/>
+                    <div className='flex justify-center items-center flex-col w-[700px] max-w-full mt-4'>
+                        <Bluebtn hover={'hover:bg-blue-300'} color={'bg-[#62ADFF]'} text={"증상 입력하기"} />
+                        <Bluebtn hover={'hover:bg-[#2F4FA5]'} color={'bg-[#092A82]'} text={"분석 기록 / 예약 정보 확인하기"} />
                     </div>
                 </div>
             </section>
-        </>
+        </div>
     );
 }
 
