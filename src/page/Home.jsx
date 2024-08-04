96d8097ff5a55909ed76be8175b155a362229dc3
jjh4450 <jjh4450git@gmail.com>
2024-08-04 22:22:39 +0900

    feat: home 수정

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import loading from '../assets/lz8ihbkj.lottie';
-import { useMediaQuery } from 'react-responsive';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import Lottie from "react-lottie-player";
+import loading from '../assets/HEALIX.json';
+import {Link} from 'react-router-dom';
+import WhiteButton from "../components/WhiteButton.jsx";
 
 function Start() {
-    const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
     const [selectedParts, setSelectedParts] = useState([]);
     const isAnySelected = selectedParts.length > 0;
 
@@ -19,26 +18,11 @@ function Start() {
         );
     };
 
-    const Whitebtn = ({ text }) => {
-        const isSelected = selectedParts.includes(text);
-
-        return (
-            <button
-                className={`hover:bg-blue-300 transition ease-in-out flex justify-center items-center w-48 h-14 ${isMobile ? 'm-2' : 'm-4'} rounded-full border border-solid border-healix-btn-border ${
-                    isSelected ? 'bg-healix-blue text-white' : isAnySelected ? 'bg-healix-gray text-black' : 'bg-white text-black'
-                } ${isMobile ? 'text-base' : 'text-lg'}`}
-                onClick={() => handleWhiteBtnClick(text)}
-            >
-                {text}
-            </button>
-        );
-    };
-
-    const Bluebtn = ({ hover, color, text, link, disabled }) => {
+    const Bluebtn = ({hover, color, text, link, disabled}) => {
         const isBlue = color === 'bg-healix-skyblue';
         const buttonClassNames = `${hover} transition ease-in-out flex justify-center items-center rounded-full w-full h-16 m-2 ${
             isBlue ? isAnySelected ? 'bg-healix-blue' : color : color
-        } ${isMobile ? 'text-xl' : 'text-2xl'}`;
+        } text-xl lg:text-2xl`;
 
         const buttonContent = (
             <button className={buttonClassNames} disabled={disabled}>
@@ -63,39 +47,44 @@ function Start() {
 
     return (
         <div className="bg-healix-gray">
-            <div className={`w-full flex justify-center ${isMobile ? 'h-[300px]' : 'h-[400px]'}`}>
-                <div className={isMobile ? 'w-[300px] h-full' : 'w-[400px] h-full'}>
-                    <DotLottieReact
-                        src={loading}
+            <div className="w-full flex justify-center h-[300px]">
+                {/*<div className="w-[300px] h-full">*/}
+                    <Lottie
                         loop
-                        autoplay
-                        segments={[0, 12]}
-                        mode="reverse"
-                        useFrameInterpolation={false}
+                        animationData={loading}
+                        play
+                        // style={{ width: , height: 150 }}
                     />
-                </div>
+                {/*</div>*/}
             </div>
 
             <section className="body-font text-white flex justify-center items-center h-full">
                 <div className="flex flex-col justify-between items-center w-11/12 h-full">
-                    <div className="flex flex-col text-black items-center w-full h-40">
-                        <h1 className={`font-semibold ml-2 mt-2 ${isMobile ? 'text-2xl' : 'text-4xl'}`}>
-                            증상을 통해 <br />내 몸의 문제를 분석해드려요
+                    <div className="flex flex-col text-black items-center w-full mb-5">
+                        <h1 className="font-semibold ml-2 mt-2 text-2xl lg:text-4xl ">
+                            증상을 통해<br/> 내 몸의 문제를 분석해드려요
                         </h1>
-                        <p className="mt-5">증상이 있는 부위를 선택해주세요.(중복가능)</p>
+                        <p className="mt-2">증상이 있는 부위를 선택해주세요.(중복가능)</p>
                     </div>
                     <div className="flex flex-col justify-center items-center w-full flex-grow">
                         {bodyPart.map((part, index) => (
                             <div key={index} className="flex justify-center items-center w-full h-16 m-2">
                                 {part.map((item, index) => (
-                                    <Whitebtn key={index} text={item} />
+                                    <WhiteButton key={index} onClick={() => handleWhiteBtnClick(item)}
+                                                 selected={selectedParts.includes(item)}
+                                                 className="w-24 sm:w-48 h-14 lg:m-4"
+                                    >
+                                        {item}
+                                    </WhiteButton>
                                 ))}
                             </div>
                         ))}
                     </div>
                     <div className="flex justify-center items-center flex-col w-[700px] max-w-full mt-4 px-2">
-                        <Bluebtn hover="hover:bg-healix-skyblue" color="bg-healix-skyblue" text="증상 입력하기" link="/text" disabled={!isAnySelected} />
-                        <Bluebtn hover="hover:bg-healix-navy-hover" color="bg-healix-navy" text="분석 기록 / 예약 정보 확인하기" link="/login" />
+                        <Bluebtn hover="hover:bg-healix-skyblue" color="bg-healix-skyblue" text="증상 입력하기" link="/text"
+                                 disabled={!isAnySelected}/>
+                        <Bluebtn hover="hover:bg-healix-navy-hover" color="bg-healix-navy" text="분석 기록 / 예약 정보 확인하기"
+                                 link="/login"/>
                     </div>
                 </div>
             </section>
